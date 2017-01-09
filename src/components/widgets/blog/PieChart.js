5e94c4eee7cf46e3aa55eea9b537e2422cae92b7
max_underthesun <max.underthesun@gmail.com>
2017-01-09 12:59:02 +0400

    add Container and Header to PieChart

--- src/components/widgets/blog/PieChart.js
+++ src/components/widgets/blog/PieChart.js
@@ -1,7 +1,8 @@
-import React, { DOM } from 'react';
+import React, { DOM, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
 import c3 from 'c3';
+import { Container, Header } from 'semantic-ui-react';
 
 class PieChart extends React.Component {
   componentDidMount() {
@@ -24,9 +25,56 @@ class PieChart extends React.Component {
   }
 
   render() {
-    return DOM.div({ ref: 'chart' });
+    // return DOM.div({ ref: 'chart' });
     // return DOM.div({ id: 'chart' });
+    return React.createElement(
+      Container,
+      { style: chartContainerStyle, text: true },
+      React.createElement(
+        Header,
+        { as: 'h2', style: headerStyle },
+        'Likes Chart'
+      ),
+      DOM.div({ ref: 'chart', style: chartDivStyle })
+    );
+    // return React.createElement(
+    //   PieCartBox,
+    //   { ref: 'chart' }
+    // );
   }
 }
+
+PieChart.propTypes = {
+  columns: PropTypes.array,
+};
+
+// const PieCartBox = ({ ref }) => (
+//   React.createElement(
+//     Container,
+//     { style: chartContainerStyle, text: true },
+//     React.createElement(
+//       Header,
+//       { as: 'h2', style: headerStyle },
+//       'Likes Chart'
+//     ),
+//     DOM.div({ ref, style: chartDivStyle })
+//   )
+// );
+
+const headerStyle = {
+  margin: '25px',
+  marginTop: '30px',
+  color: '#444',
+};
+
+const chartContainerStyle = {
+  backgroundColor: '#888',
+  padding: '15px'
+};
+
+const chartDivStyle = {
+  backgroundColor: '#bbb',
+  margin: '25px'
+};
 
 export default PieChart;
