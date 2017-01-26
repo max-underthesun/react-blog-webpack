f8ad6be7e75b470ecd81f154be3f1f87d7fe6f4d
max_underthesun <max.underthesun@gmail.com>
2017-01-26 12:20:31 +0400

    add fetchPosts() to BlogPage, fetching list of items from the external server for index

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,17 +1,23 @@
 import React, { DOM } from 'react';
 import update from 'immutability-helper';
 import { bind, map } from 'lodash';
+import request from 'superagent';
 
-import { items as staticItems } from 'constants/static/items';
+// import { items as staticItems } from 'constants/static/items';
 import BlogList from 'components/widgets/blog/List';
 import PieChart from 'components/widgets/blog/PieChart';
 
 class BlogPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: staticItems };
+    // this.state = { items: staticItems };
+    this.state = { items: [] };
 
     this.like = bind(this.like, this);
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
   }
 
   like(id) {
@@ -24,6 +30,14 @@ class BlogPage extends React.Component {
         {[index]: { meta: { count: { $apply(x) { return x + 1; } } } } }
       )
     });
+  }
+
+  fetchPosts() {
+    request.get(
+      'http://localhost:3001',
+      {},
+      (err, res) => this.setState({ items: res.body })
+    );
   }
 
   render() {
