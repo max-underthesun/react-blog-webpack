f16a869299322377165bd9fc2ef94063f89315ce
max_underthesun <max.underthesun@gmail.com>
2017-02-10 10:03:31 +0400

    extract GoBackButton, PageFooter and PageHeader from MainLayout

--- src/components/layouts/MainLayout.js
+++ src/components/layouts/MainLayout.js
@@ -1,111 +1,23 @@
-import React, { PropTypes, DOM } from 'react';
-import { Button, Segment, Container, Header, Grid, Menu } from 'semantic-ui-react';
-import { bind, assign } from 'lodash';
+import React, { PropTypes } from 'react';
+import { Container } from 'semantic-ui-react';
 
-import Link from 'components/elements/Link';
-import history from 'helpers/history';
+import GoBackButton from 'components/layouts/elements/GoBackButton';
+import PageFooter from 'components/layouts/widgets/PageFooter';
+import PageHeader from 'components/layouts/widgets/PageHeader';
 
 const MainLayout = ({ children }) => (
   React.createElement(
     Container,
     {},
-    React.createElement(Logo),
+    React.createElement(PageHeader),
     React.createElement(GoBackButton),
     children,
-    React.createElement(Footer)
+    React.createElement(PageFooter)
   )
 );
 
 MainLayout.propTypes = {
   children: PropTypes.node
 };
-
-const GoBackButton = () => (
-  // React.createElement(
-  //   Button,
-  //   { onClick: () => history.goBack(), inverted: true, color: 'orange' },
-  //   'Back'
-  // )
-  React.createElement(
-    Grid,
-    {},
-    React.createElement(
-      Grid.Row,
-      {},
-      React.createElement(
-        Grid.Column,
-        {},
-        React.createElement(
-          Button,
-          { onClick: () => history.goBack(), inverted: true, color: 'orange', floated: 'right' },
-          'Back'
-        )
-      )
-    )
-  )
-);
-
-const Logo = () => (
-// class Logo extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     // this.state = { activeItem: 'home'};
-//     // this.handleItemClick = bind(this.handleItemClick, this);
-//   }
-
-  // handleItemClick(e, { name }) {
-  //   this.setState({ activeItem: name });
-  // }
-
-  // render() {
-    // const { activeItem } = this.state;
-    React.createElement(
-      Menu,
-      { inverted: true, size: 'huge', color: 'grey' },
-      React.createElement(
-        Menu.Item,
-        { header: true },
-        // { header: true, name: 'home', active: (activeItem === 'home'), onClick: this.handleItemClick },
-        React.createElement(
-          Link,
-          { to: '/' },
-          'ThinknetikaBlog'
-        )
-      ),
-      React.createElement(
-        Menu.Item,
-        {},
-        // { name: 'about', active: (activeItem === 'about'), color: 'green', onClick: this.handleItemClick },
-        React.createElement(
-          Link,
-          { to: '/about' },
-          'About'
-        )
-      )
-    )
-  // }
-// }
-  // React.createElement(
-  //   Segment,
-  //   {},
-  //   React.createElement(
-  //     Header,
-  //     { as: 'h2'},
-  //     React.createElement(
-  //       Link,
-  //       { to: '/' },
-  //       'ThinknetikaBlog'
-  //     )
-  //   )
-  // )
-);
-
-const Footer = () => (
-  React.createElement(
-    Segment,
-    {},
-    'Powered By React Course.'
-  )
-);
 
 export default MainLayout;
---
+++ src/components/layouts/widgets/PageFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+
+const PageFooter = () => (
+  React.createElement(
+    Segment,
+    {},
+    'Powered By React Course.'
+  )
+);
+
+export default PageFooter;
