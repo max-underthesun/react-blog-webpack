92efcb8f5b625e8033fd529bc125d782aa734b35
max_underthesun <max.underthesun@gmail.com>
2017-02-10 10:07:01 +0400

    clear App.js from commented code

--- src/App.js
+++ src/App.js
@@ -2,38 +2,8 @@ import React from 'react';
 import { Router, browserHistory } from 'react-router';
 
 import routes from 'routes';
-// import MainLayout from 'components/layouts/MainLayout';
-// import BlogPage from 'components/BlogPage';
-
-// import MainLayout from 'components/layouts/MainLayout';
-// import BlogPage from 'components/BlogPage';
-// import Post from 'components/Post';
-// import { postsPath } from 'helpers/routes';
-// const Index = {
-//   path: '/',
-//   component: BlogPage
-// };
-//
-// const PostRoute = {
-//   // path: '/posts/:id',
-//   path: postsPath(),
-//   component: Post
-// };
-//
-// const routes = {
-//   component: MainLayout,
-//   childRoutes: [
-//     Index,
-//     PostRoute
-//   ]
-// };
 
 const App = () => (
-  // React.createElement(
-  //   MainLayout,
-  //   {},
-  //   React.createElement(BlogPage)
-  // )
   React.createElement(
     Router,
     { history: browserHistory, routes }
