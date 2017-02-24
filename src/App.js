94062937410b6c36b7be9bb5642717a72e00b077
max_underthesun <max.underthesun@gmail.com>
2017-02-24 18:07:38 +0400

    add store, use Provider in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 
 import routes from 'routes';
+import store from 'store';
 
 const App = () => (
   React.createElement(
-    Router,
-    { history: browserHistory, routes }
+    Provider,
+    { store },
+    React.createElement(
+      Router,
+      { history: browserHistory, routes }
+    )
   )
 );
 
