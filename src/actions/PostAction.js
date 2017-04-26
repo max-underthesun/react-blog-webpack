05521c315b0fb30d2f727fbd33340ee948199fba
max_underthesun <max.underthesun@gmail.com>
2017-04-26 17:36:39 +0400

    rewrite PostAction

--- src/actions/PostAction.js
+++ src/actions/PostAction.js
@@ -1,32 +1,54 @@
-import request from 'superagent';
-
-import { SERVER_URL } from 'constants/ServerUrl';
+import { API_CALL } from 'constants/middleware/API';
 import * as types from 'constants/actionTypes/PostActionTypes';
 
-const requestPost = (id) => ({
-  type: types.FETCH_POST_REQUEST,
-  id
+export const fetchPost = (id) => ({
+  [API_CALL]: {
+    endpoint: `/posts/${id}`,
+    query: {},
+    method: 'GET',
+    types: [
+      types.FETCH_POST_REQUEST,
+      types.FETCH_POST_SUCCESS,
+      types.FETCH_POST_ERROR
+    ]
+  }
 });
 
-const errorPost = () => ({
-  type: types.FETCH_POST_ERROR
-});
+// export const addLike = (id) => ({
+//   type: types.ADD_LIKE,
+//   id
+// });
 
-const receivePost = (response) => ({
-  type: types.FETCH_POST_SUCCESS,
-  response
-});
 
-export function fetchPost(id) {
-  return (dispatch) => {
-    dispatch(requestPost(id));
-
-    return request
-      .get(`${SERVER_URL}/posts/${id}`)
-      .end(
-        (err, response) => {
-          err ? dispatch(errorPost()) : dispatch(receivePost(response.body));
-        }
-      );
-  };
-}
+// import request from 'superagent';
+//
+// import { SERVER_URL } from 'constants/ServerUrl';
+// import * as types from 'constants/actionTypes/PostActionTypes';
+//
+// const requestPost = (id) => ({
+//   type: types.FETCH_POST_REQUEST,
+//   id
+// });
+//
+// const errorPost = () => ({
+//   type: types.FETCH_POST_ERROR
+// });
+//
+// const receivePost = (response) => ({
+//   type: types.FETCH_POST_SUCCESS,
+//   response
+// });
+//
+// export function fetchPost(id) {
+//   return (dispatch) => {
+//     dispatch(requestPost(id));
+//
+//     return request
+//       .get(`${SERVER_URL}/posts/${id}`)
+//       .end(
+//         (err, response) => {
+//           err ? dispatch(errorPost()) : dispatch(receivePost(response.body));
+//         }
+//       );
+//   };
+// }
