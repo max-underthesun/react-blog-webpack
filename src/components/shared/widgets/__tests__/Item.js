2261b0841478570f08aaf810dc61408117491016
max_underthesun <max.underthesun@gmail.com>
2017-06-03 18:05:25 +0400

    add BlogItem tests 'renders without craching' and 'should render the title'

---
+++ src/components/shared/widgets/__tests__/Item.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import createStore from 'store';
+
+import Link from 'components/shared/elements/Link';
+import BlogItem from '../BlogItem';
+
+describe('BlogItem', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const store = createStore({});
+    // ReactDOM.render(<BlogItem />, div);
+    ReactDOM.render(<Provider store={store}><BlogItem /></Provider>, div);
+  });
+
+  describe('render', () => {
+    it('should render the title', () => {
+      const itemProps = { title: 'Hello, World!', id: 5 };
+
+      // const item = shallow(<BlogItem item={itemProps} />);
+      const item = shallow(
+        React.createElement(BlogItem, itemProps)
+      );
+      const header = <Link to="/posts/5">Hello, World!</Link>;
+
+      expect(item.contains(header)).toEqual(true);
+    });
+  });
+});
